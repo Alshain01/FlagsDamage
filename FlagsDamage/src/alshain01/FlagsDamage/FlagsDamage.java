b1ab058a7cb2d7b6800ff47c4b5f4eca0122fada
Kevin Seiden <alshain01@suddenlink.net>
2013-11-05 15:51:12 -0600

    Code cleanup and optimization

--- FlagsDamage/src/alshain01/FlagsDamage/FlagsDamage.java
+++ FlagsDamage/src/alshain01/FlagsDamage/FlagsDamage.java
@@ -20,9 +20,10 @@
 
  Notice — For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page.
  http://creativecommons.org/licenses/by-nc/3.0/
-*/
+ */
 
 package alshain01.FlagsDamage;
+
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
@@ -35,65 +36,25 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import alshain01.Flags.Flags;
+import alshain01.Flags.Director;
 import alshain01.Flags.Flag;
+import alshain01.Flags.Flags;
 import alshain01.Flags.ModuleYML;
 import alshain01.Flags.Registrar;
-import alshain01.Flags.Director;
 import alshain01.Flags.area.Area;
 import alshain01.Flags.area.Siege;
 
 /**
- * Flags - Damage
- * Module that adds damage flags to the plug-in Flags.
+ * Flags - Damage Module that adds damage flags to the plug-in Flags.
  * 
  * @author Alshain01
  */
 public class FlagsDamage extends JavaPlugin {
-	/**
-	 * Called when this module is enabled
-	 */
-	@Override
-	public void onEnable(){
-		PluginManager pm =  Bukkit.getServer().getPluginManager();
-
-		if(!pm.isPluginEnabled("Flags")) {
-		    this.getLogger().severe("Flags was not found. Shutting down.");
-		    pm.disablePlugin(this);
-		}
-		
-		// Connect to the data file
-		ModuleYML dataFile = new ModuleYML(this, "flags.yml");
-		
-		// Register with Flags
-		Registrar flags = Flags.getRegistrar();
-		for(String f : dataFile.getModuleData().getConfigurationSection("Flag").getKeys(false)) {
-			ConfigurationSection data = dataFile.getModuleData().getConfigurationSection("Flag." + f);
-			
-			// We don't want to register flags that aren't supported.
-			// It would just muck up the help menu.
-			// Null value is assumed to support all versions.
-			String api = data.getString("MinimumAPI");  
-			if(api != null && !Flags.checkAPI(api)) { continue; }
-			
-			// The description that appears when using help commands.
-			String desc = data.getString("Description");
-			
-			// Register it!  (All flags are defaulting to true in this module)
-			// Be sure to send a plug-in name or group description for the help command!
-			// It can be this.getName() or another string.
-			flags.register(f, desc, true, "Damage");
-		}
-		
-		// Load plug-in events and data
-		Bukkit.getServer().getPluginManager().registerEvents(new EntityDamageListener(), this);
-	}
-	
 	/*
 	 * The event handler for the flags we created earlier
 	 */
@@ -101,110 +62,176 @@ public class FlagsDamage extends JavaPlugin {
 		@EventHandler(ignoreCancelled = true)
 		private void onEntityDamage(EntityDamageEvent e) {
 			// If the damage is not a to a player, we do nothing.
-			if(!(e.getEntity() instanceof Player)) { return; }
-			
-			if(e.getCause() == DamageCause.ENTITY_ATTACK || e.getCause() == DamageCause.ENTITY_EXPLOSION || e.getCause() == DamageCause.PROJECTILE) {
-				// Handled by subclass events EntityDamageByEntity so that we can retrieve the damager.
+			if (!(e.getEntity() instanceof Player)) {
 				return;
 			}
-			
-			Flag flag = null;
-			Registrar flags = Flags.getRegistrar();
-			
-			if (e.getCause() == DamageCause.BLOCK_EXPLOSION) {
-				flag = flags.getFlag("DamageBlockExplode");
-			} else if (e.getCause() == DamageCause.CONTACT) {
-				flag = flags.getFlag("DamageBlockContact");
-			} else if (e.getCause() == DamageCause.DROWNING) {
-				flag = flags.getFlag("DamageDrown");
-			} else if (e.getCause() == DamageCause.FALL) {
-				flag = flags.getFlag("DamageFall");
-			} else if (e.getCause() == DamageCause.FIRE) {
-				flag = flags.getFlag("DamageFire");
-			} else if (e.getCause() == DamageCause.FIRE_TICK) {
-				flag = flags.getFlag("DamageBurn");
-			} else if (e.getCause() == DamageCause.LAVA) {
-				flag = flags.getFlag("DamageLava");
-			} else if (e.getCause() == DamageCause.LIGHTNING) {
-				flag = flags.getFlag("DamageLightning");
-			} else if (e.getCause() == DamageCause.MAGIC) {
-				flag = flags.getFlag("DamageMagic");
-			} else if (e.getCause() == DamageCause.MELTING) {
-				flag = flags.getFlag("DamageMelting");
-			} else if (e.getCause() == DamageCause.POISON) {
-				flag = flags.getFlag("DamagePoison");
-			} else if (e.getCause() == DamageCause.STARVATION) {
-				flag = flags.getFlag("DamageStarve");
-			} else if (e.getCause() == DamageCause.SUFFOCATION) {
-				flag = flags.getFlag("DamageSuffocate");
-			} else if (e.getCause() == DamageCause.SUICIDE) {
-				flag = flags.getFlag("DamageSuicide");
-			} else if (e.getCause() == DamageCause.VOID) {
-				flag = flags.getFlag("DamageVoid");
-			} else if (Flags.checkAPI("1.4.5")
-					&& e.getCause() == DamageCause.FALLING_BLOCK) {
-				flag = flags.getFlag("DamageBlockFall");
-			} else if(Flags.checkAPI("1.4.5")
-					&& e.getCause() == DamageCause.WITHER) {
-				flag = flags.getFlag("DamageWither");
-			} else if(Flags.checkAPI("1.5.2")
-					&& e.getCause() == DamageCause.THORNS) {
-				flag = flags.getFlag("DamageThorns");
-			} else {
-				flag = flags.getFlag("DamageOther");
+
+			if (e.getCause() == DamageCause.ENTITY_ATTACK
+					|| e.getCause() == DamageCause.ENTITY_EXPLOSION
+					|| e.getCause() == DamageCause.PROJECTILE) {
+				// Handled by subclass events EntityDamageByEntity so that we
+				// can retrieve the damager.
+				return;
 			}
-		
-			if (flag != null) { // Always guard this, even when it really can't happen.
+
+			Flag flag = null;
+			final Registrar flags = Flags.getRegistrar();
+
+			switch (e.getCause()) {
+			case BLOCK_EXPLOSION:
+				flag = flags.getFlag("DamageBlockExplode");
+			case CONTACT:
+				flag = flags.getFlag("DamageBlockContact");
+			case DROWNING:
+				flag = flags.getFlag("DamageDrown");
+			case FALL:
+				flag = flags.getFlag("DamageFall");
+			case FIRE:
+				flag = flags.getFlag("DamageFire");
+			case FIRE_TICK:
+				flag = flags.getFlag("DamageBurn");
+			case LAVA:
+				flag = flags.getFlag("DamageLava");
+			case LIGHTNING:
+				flag = flags.getFlag("DamageLightning");
+			case MAGIC:
+				flag = flags.getFlag("DamageMagic");
+			case MELTING:
+				flag = flags.getFlag("DamageMelting");
+			case POISON:
+				flag = flags.getFlag("DamagePoison");
+			case STARVATION:
+				flag = flags.getFlag("DamageStarve");
+			case SUFFOCATION:
+				flag = flags.getFlag("DamageSuffocate");
+			case SUICIDE:
+				flag = flags.getFlag("DamageSuicide");
+			case VOID:
+				flag = flags.getFlag("DamageVoid");
+			default:
+				if (Flags.checkAPI("1.4.5")
+						&& e.getCause() == DamageCause.FALLING_BLOCK) {
+					flag = flags.getFlag("DamageBlockFall");
+				} else if (Flags.checkAPI("1.4.5")
+						&& e.getCause() == DamageCause.WITHER) {
+					flag = flags.getFlag("DamageWither");
+				} else if (Flags.checkAPI("1.5.2")
+						&& e.getCause() == DamageCause.THORNS) {
+					flag = flags.getFlag("DamageThorns");
+				} else {
+					flag = flags.getFlag("DamageOther");
+				}
+			}
+
+			// Always guard this, even when it really can't happen.
+			if (flag != null) { 
 				e.setCancelled(!Director.getAreaAt(e.getEntity().getLocation()).getValue(flag, false));
 			}
 		}
-		
-		@EventHandler(ignoreCancelled = true) 
-		private void onEntityDamagedByEntity(EntityDamageByEntityEvent e){
+
+		@EventHandler(ignoreCancelled = true)
+		private void onEntityDamagedByEntity(EntityDamageByEntityEvent e) {
 			// If the damage is not a to a player, we do nothing.
-			if (!(e.getEntity() instanceof Player)){ return; }
-			
+			if (!(e.getEntity() instanceof Player)) {
+				return;
+			}
+
 			Flag flag = null;
-			Registrar flags = Flags.getRegistrar();
-			
-			Entity damager = e.getDamager();
-			if (damager instanceof Monster
-					|| (damager instanceof Projectile && ((Projectile)damager).getShooter() instanceof Monster)) {
+			final Registrar flags = Flags.getRegistrar();
+
+			final Entity damager = e.getDamager();
+			if (damager instanceof Monster || damager instanceof Projectile
+					&& ((Projectile) damager).getShooter() instanceof Monster) {
 				flag = flags.getFlag("DamageMonster");
-			} else if (damager instanceof Player 
-					|| (damager instanceof Projectile && ((Projectile)damager).getShooter() instanceof Player)) {
-				if (!Director.inPvpCombat((Player)e.getEntity())) {
-					// Don't interfere with a battle, you can't attack and then retreat to a protected area (that's cheating)
-					// Uses GP's timer (15 second default).  Not supported by other systems.
-					Area area = Director.getAreaAt(e.getEntity().getLocation());
-					if(!(area instanceof Siege) || !((Siege)area).isUnderSiege()) {
-						// If your under siege, your on your own.  That's part of the game.
+			} else if (damager instanceof Player
+					|| damager instanceof Projectile
+					&& ((Projectile) damager).getShooter() instanceof Player) {
+				if (!Director.inPvpCombat((Player) e.getEntity())) {
+					// Don't interfere with a battle, you can't attack and then
+					// retreat to a protected area (that's cheating)
+					// Uses GP's timer (15 second default). Not supported by
+					// other systems.
+					final Area area = Director.getAreaAt(e.getEntity().getLocation());
+					if (!(area instanceof Siege) || !((Siege) area).isUnderSiege()) {
+						// If your under siege, your on your own.
+						// That's part of the game.
 						// Only supported by GP.
 						flag = flags.getFlag("Pvp");
 					}
 				}
 			} else {
-				flag = flags.getFlag("DamageOther"); // Not really sure how you got here, but if you did...
+				// Not really sure how you got here, but if you did...
+				flag = flags.getFlag("DamageOther");
 			}
-			
-			if(flag != null) { // Always guard this, even when it really can't happen.
+
+			// Always guard this, even when it really can't happen.
+			if (flag != null) { 
 				e.setCancelled(!Director.getAreaAt(e.getEntity().getLocation()).getValue(flag, false));
 			}
 		}
-		
+
 		@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-		private void onPotionSplash(PotionSplashEvent e){
-			Flag flag = Flags.getRegistrar().getFlag("PotionSplash");
-			if(flag == null) { return; }
-			
-			for (LivingEntity entity : e.getAffectedEntities()) {
-				if (!(entity instanceof Player)) { continue; }
-				if (Director.getAreaAt(e.getEntity().getLocation()).getValue(flag, false)) {
+		private void onPotionSplash(PotionSplashEvent e) {
+			final Flag flag = Flags.getRegistrar().getFlag("PotionSplash");
+			if (flag == null) {
+				return;
+			}
+
+			for (final LivingEntity entity : e.getAffectedEntities()) {
+				if (!(entity instanceof Player)) {
+					continue;
+				}
+				if (Director.getAreaAt(e.getEntity().getLocation()).getValue(
+						flag, false)) {
 					// Essentially cancels it.
-					// Only way to cancel on a player by player basis instead of the whole effect.
+					// Only way to cancel on a player by player basis instead of
+					// the whole effect.
 					e.setIntensity(entity, 0);
 				}
 			}
 		}
+	}
+
+	/**
+	 * Called when this module is enabled
+	 */
+	@Override
+	public void onEnable() {
+		final PluginManager pm = Bukkit.getServer().getPluginManager();
+
+		if (!pm.isPluginEnabled("Flags")) {
+			getLogger().severe("Flags was not found. Shutting down.");
+			pm.disablePlugin(this);
+		}
+
+		// Connect to the data file
+		final ModuleYML dataFile = new ModuleYML(this, "flags.yml");
+
+		// Register with Flags
+		final Registrar flags = Flags.getRegistrar();
+		for (final String f : dataFile.getModuleData().getConfigurationSection("Flag").getKeys(false)) {
+			final ConfigurationSection data = dataFile.getModuleData().getConfigurationSection("Flag." + f);
+
+			// We don't want to register flags that aren't supported.
+			// It would just muck up the help menu.
+			// Null value is assumed to support all versions.
+			final String api = data.getString("MinimumAPI");
+			if (api != null && !Flags.checkAPI(api)) {
+				continue;
+			}
+
+			// The description that appears when using help commands.
+			final String desc = data.getString("Description");
+
+			// Register it! (All flags are defaulting to true in this module)
+			// Be sure to send a plug-in name or group description for the help
+			// command!
+			// It can be this.getName() or another string.
+			flags.register(f, desc, true, "Damage");
+		}
+
+		// Load plug-in events and data
+		Bukkit.getServer().getPluginManager()
+				.registerEvents(new EntityDamageListener(), this);
 	}
 }
