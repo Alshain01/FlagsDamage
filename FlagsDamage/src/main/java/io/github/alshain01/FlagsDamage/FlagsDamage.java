3e6bb12da4b8de5859b75d5d34682717689c40d9
Kevin Seiden <alshain01@suddenlink.net>
2013-12-01 00:53:09 -0600

    Fix broken package

--- FlagsDamage/src/main/java/io/github/alshain01/FlagsDamage/FlagsDamage.java
+++ FlagsDamage/src/main/java/io/github/alshain01/FlagsDamage/FlagsDamage.java
@@ -3,26 +3,26 @@
  This works is licensed under the Creative Commons Attribution-NonCommercial 3.0
 
  You are Free to:
-    to Share — to copy, distribute and transmit the work
-    to Remix — to adapt the work
+    to Share ï¿½ to copy, distribute and transmit the work
+    to Remix ï¿½ to adapt the work
 
  Under the following conditions:
-    Attribution — You must attribute the work in the manner specified by the author (but not in any way that suggests that they endorse you or your use of the work).
-    Non-commercial — You may not use this work for commercial purposes.
+    Attribution ï¿½ You must attribute the work in the manner specified by the author (but not in any way that suggests that they endorse you or your use of the work).
+    Non-commercial ï¿½ You may not use this work for commercial purposes.
 
  With the understanding that:
-    Waiver — Any of the above conditions can be waived if you get permission from the copyright holder.
-    Public Domain — Where the work or any of its elements is in the public domain under applicable law, that status is in no way affected by the license.
-    Other Rights — In no way are any of the following rights affected by the license:
+    Waiver ï¿½ Any of the above conditions can be waived if you get permission from the copyright holder.
+    Public Domain ï¿½ Where the work or any of its elements is in the public domain under applicable law, that status is in no way affected by the license.
+    Other Rights ï¿½ In no way are any of the following rights affected by the license:
         Your fair dealing or fair use rights, or other applicable copyright exceptions and limitations;
         The author's moral rights;
         Rights other persons may have either in the work itself or in how the work is used, such as publicity or privacy rights.
 
- Notice — For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page.
+ Notice ï¿½ For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page.
  http://creativecommons.org/licenses/by-nc/3.0/
  */
 
-package alshain01.FlagsDamage;
+package io.github.alshain01.FlagsDamage;
 
 import io.github.alshain01.Flags.Flag;
 import io.github.alshain01.Flags.Flags;
